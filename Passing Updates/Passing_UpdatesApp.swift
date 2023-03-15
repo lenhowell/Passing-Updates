b356f29f4551d922b8292f6d595e1c20084eaa3c
Lenard Howell <len.howell@gmail.com>
2023-03-15 14:15:06 -0400

    Initial Commit

---
+++ Passing Updates/Passing_UpdatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Passing_UpdatesApp.swift
+//  Passing Updates
+//
+//  Created by Lenard Howell on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Passing_UpdatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
